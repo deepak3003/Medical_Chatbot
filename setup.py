192a7f3d54b1cb2a14c071ff4433a76bc7f587c7
deepak3003 <deepaksingh76152@gmail.com>
2025-08-09 21:57:41 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='medical_chatbot',
+    version='0.1.0',
+    author='Deepak Singh',
+    author_email='deepaksingh76152@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+
+)
